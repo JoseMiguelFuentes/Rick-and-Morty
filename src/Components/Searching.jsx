391f5af680db3b07569dfff9143a711ae2ad6d33
JoseMiguelFuentes <jomifuver@hotmail.com.com>
2022-07-12 00:10:58 -0500

    Improvements for the input and design

--- src/Components/Searching.jsx
+++ src/Components/Searching.jsx
@@ -11,18 +11,37 @@ const Searching = ({setLocationId, locationId, getApiData})=> {
   const setNewLocationId =()=>{
   setLocationId(newId)
   getApiData()
+  setNewId('')
   }
 
- 
+ /*** Validate that  input text is not numbers  ***/
+  const validate = (e)=>{
+    let numbers = '0123456789'
+    let currentletter = e
+    let fullstring = ``
+    for(let num in numbers){
+      if( num === currentletter[currentletter.length -1] && currentletter.length > 1){
+        fullstring += e
+      }
+      if(num === currentletter){
+        fullstring = e
+      }
+      setNewId(fullstring)
+    }
+    /*** Validate that  input text is not numbers  ***/
+  }
 
-
-
+  
   
     return(
       <>
         <article className="search--button">
-          <input className="newId__input" type="text" value={newId} onChange={event => setNewId(event.target.value)} placeholder='Type a location ID between 1-126'/>
-          <button className="newId__button" onClick={setNewLocationId}>Search</button>
+          <input className="newId__input" type="text" value={newId} onChange={ e =>
+            validate(e.target.value)
+            } onKeyDown={e => {e.key === 'Enter' && setNewLocationId()}}
+             placeholder='Type a location ID between 1-126'/>
+
+          <button className="newId__button" onClick={setNewLocationId} >Search</button>
         </article>
      </>
     )
